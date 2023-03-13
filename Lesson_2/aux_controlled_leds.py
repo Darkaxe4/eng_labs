2d40e4208ad47bf5657cbece0b267654f4383494
darkaxe4 <makarov-ilya2812@yandex.ru>
2023-03-13 15:12:10 +0300

    repaired file struct

--- Lesson_2/aux_controlled_leds.py
+++ Lesson_2/aux_controlled_leds.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 
 
-AUX_PORTS = [2, 3, 14, 15, 18, 27, 23, 22]
-LED_PORTS = [24, 25, 8, 7, 12, 16, 20, 21]
+AUX_PORTS = [2, 3, 14, 15, 18, 27, 23, 22] #some input ports
+LED_PORTS = [24, 25, 8, 7, 12, 16, 20, 21] #some output ports
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PORTS, GPIO.OUT)
